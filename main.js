87e68e4298e69a889213d1a5aa2bebb9d552a0de
abuhorainhero <abuhorain2016@gmail.com>
2020-08-23 00:08:56 +0600

    song name na dile alert dibe

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 
 document.getElementById('search').addEventListener("click", function(){
     const songName = document.getElementById("song-name").value;
-    fetch('https://api.lyrics.ovh/suggest/'+songName)
-    .then(response => response.json())
-    .then(data => {
-        const song_title = data.data.map(song_title => song_title.title);
-        const song_name = data.data.map(song_name => song_name.artist.name);
-        var template = " ";
-        for (let i = 0; i < 10; i++){
-            const title = song_title[i];
-            const name = song_name[i];
-            const parentItem = `<div class="single-result row align-items-center my-3 p-3">
-            <div class="col-md-9">
-                <h3 >${title}</h3>
-                <p class="author lead"> Album by <span>${name}</span></p>
-            </div>
-            <div class="col-md-3 text-md-right text-center">
-                <button onclick="getLyrics('${name}', '${title}')" class="btn btn-success">Get Lyrics</button>
-            </div>
-        </div>`
-            template += parentItem;
-        }
-        document.getElementById("parent").innerHTML = template;
-    })
-    .catch(error => console.log(error));
+    if (songName === "") {
+        alert("Please Enter song Name");
+    }
+    else {
+        fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+        .then(response => response.json())
+        .then(data => {
+            const song_title = data.data.map(song_title => song_title.title);
+            const song_name = data.data.map(song_name => song_name.artist.name);
+            var template = " ";
+            for (let i = 0; i < 10; i++){
+                const title = song_title[i];
+                const name = song_name[i];
+                const parentItem = `<div class="single-result row align-items-center my-3 p-3">
+                <div class="col-md-9">
+                    <h3 >${title}</h3>
+                    <p class="author lead"> Album by <span>${name}</span></p>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                    <button onclick="getLyrics('${name}', '${title}')" class="btn btn-success">Get Lyrics</button>
+                </div>
+            </div>`
+                template += parentItem;
+            }
+            document.getElementById("parent").innerHTML = template;
+        })
+        .catch(error => console.log(error));
+    }
 })
 
 // Lyrics API
